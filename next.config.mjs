b82797c596ec53dd3efae6efb20f6a3677894312
hibiskush <endercraft04vn@gmail.com>
2024-09-20 00:47:06 +0700

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "kpvlhlxmswfqritpomty.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
